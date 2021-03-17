a8bad6875ff68655b6e5ee0191fc5813bd9c841a
joel dominic lobo <joe888dom@gmail.com>
2021-03-17 14:34:36 +0530

    created components and embedded them in the App component

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ExpenseTotal = () => {
+  return (
+    <div className="alert alert-primary">
+      <span>Spent so far: </span>
+    </div>
+  );
+};
+export default ExpenseTotal;
